fa886042ed3dbcd7d7388ccbf2e59960bd30b9c0
Mustafa Timbawala <mm.timbawala@gmail.com>
2025-06-24 08:06:26 -0400

    Adjusted Membercard components and Teams Page

--- frontend/src/components/MemberCard.js
+++ frontend/src/components/MemberCard.js
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { useState } from "react"; 
+import { MdLocationPin } from "react-icons/md";
 
 export default function MemberCard({ member }) {
     const [isHovered, setIsHovered] = useState(false);
 
     return ( 
-      <div className="p-8"> 
+      <div className=" font-body shadow-lg rounded-lg w-[300px] flex flex-col justify-center items-center"> 
          <div
-            className="relative w-64 h-72 overflow-hidden rounded-xl cursor-pointer"
+            className="relative w-full h-[350px] overflow-hidden rounded-lg cursor-pointer"
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
@@ -21,16 +22,17 @@ export default function MemberCard({ member }) {
 
             {/* Flag Overlay */}
             <img
-                src={member.country}
+                src={member.flag}
                 alt={`${member.name} Flag`}
                 className={`absolute top-0 left-0 w-full h-full object-cover transition-all duration-500 ${
                     isHovered ? "opacity-100 scale-100" : "opacity-0 scale-75"
                 }`}
             />
         </div> 
-        <div> 
-         <p className="text-2xl text-black">{member.name} </p> 
-         <p className="text-lg text-slate-600">{member.title}</p>
+        <div className="w-full rounded-xl flex flex-col justify-center items-start p-3"> 
+         <p className="text-xl text-bgcol font-semibold">{member.name} </p> 
+         <p className="text-lg text-highlight font-medium">{member.title}</p> 
+         <div className="flex flex-row"><MdLocationPin  /><p className="pl-1 text-lg">{member.country}</p></div>
         </div>
  
       </div>
--- frontend/src/pages/Team.js
+++ frontend/src/pages/Team.js
@@ -2,10 +2,11 @@ import MemberCard from "../components/MemberCard"
 import { MemberInfo } from "../data/MemberDictionary"
 export default function TeamPage() {
     return( 
-        <div className="flex flex-col min-h-screen"> 
+        <div className="min-h-screen mt-[20%] sm:mt-[5%] flex flex-col justify-center items-center mb-16">  
+        <h1 className="text-highlight text-3xl sm:text-5xl font-medium px-2 pb-4 sm:pb-[2%] text-center">Meet Our Team</h1> 
+        <h2 className="pb-[5%] font-medium text-xl text-center">Our association is led by a dedicated team of students who work tirelessly to create a vibrant community and organize meaningful events.</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-24 px-[5%]"> {MemberInfo.map((member)=><MemberCard key={member.id} member={member}/>)}
         </div>
-        
         </div>
     )
 } 
