e2ca373c09626bf5084537fce4e4a241f542e1f8
Mustafa Timbawala <mm.timbawala@gmail.com>
2025-07-21 15:01:56 -0400

    Updated Member List

---
+++ src/data/MemberDictionary.js
@@ -0,0 +1,34 @@
+
+
+/**
+ * @typedef {Object} MemberInfoType
+ * @property {number} id
+ * @property {string} name
+ * @property {string} title
+ * @property {string} pic
+ * @property {string} country
+ * @property {string} flag
+ */
+
+
+export const MemberInfo = [ 
+ 
+    {
+        id: 1, 
+        name:"Temitayo Adewuyi", 
+        title:"Co-President", 
+        pic:"/assets/Members/headshots/Temi.jpeg", 
+        country:"Nigeria",
+        flag:"/assets/Members/countries/Nigeria.png",
+    }, 
+    {
+        id: 2, 
+        name:"Lutifiya Inusah ", 
+        title:"Co-President", 
+        pic:"/assets/Members/headshots/Lutfiya.jpeg", 
+        country:"Ghana", 
+        flag:"/assets/Members/countries/Ghana.png",
+    }, 
+    
+
+];
